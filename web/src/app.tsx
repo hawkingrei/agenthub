93bc1f77fcfa791d4d934d52b27ed37041cc24fd
Weizhen Wang <wangweizhen@pingcap.com>
2026-02-07 17:33:07 +0800

    fix: use link for admin back

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1655,7 +1655,9 @@ function AdminPage(props: AdminProps) {
       <header>
         <h1>AgentHub Admin</h1>
         <div className="session">
-          <button onClick={() => (location.href = "/")}>Back</button>
+          <a className="icon-button" href="/" title="Back">
+            Back
+          </a>
           <span>{props.auth.username}</span>
         </div>
       </header>
