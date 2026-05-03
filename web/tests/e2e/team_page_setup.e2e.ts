67aa2aa582498f27f28df6f61a4106e12a9ba0b6
Weizhen Wang <wangweizhen@pingcap.com>
2026-05-03 15:19:08 +0800

    fix(team): remove stale onboarding e2e import

--- web/tests/e2e/team_page_setup.e2e.ts
+++ web/tests/e2e/team_page_setup.e2e.ts
@@ -15,7 +15,6 @@ import {
   mockTeamPageApis,
   openAdvancedView,
   openMainTeamAction,
-  openSelectedTeamMenu,
   openTeamFromSelector,
   selectAgentFromSidebar,
   selectPrimaryTeamEntryFromSidebar,
